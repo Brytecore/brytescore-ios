cbb6db09d9513b1d9e479cc2de42d85bf9932fc7
Emily Morehouse <emily@cuttlesoft.com>
2017-08-09 09:53:07 -0600

    [#150019614] Refactoring API Manager initialization.

--- Example/BrytescoreAPI/ViewController.swift
+++ Example/BrytescoreAPI/ViewController.swift
@@ -14,20 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var apiKeyLabel: UILabel!
 
     // Initialize the API Manager with your API key.
-    let _apiKey = "abc123"
+    let _apiManager = BrytescoreAPIManager(apiKey: "107e322f-3410-4d5b-8f43-d2bad8141771")
     let defaultAPIKeyLabel = "Your API Key:"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Initialize the API manager with our API key
-        let _apiManager = BrytescoreAPIManager(apiKey: _apiKey)
         
         // Update the API Key label to show our API key for debugging
-        apiKeyLabel.text = "\(defaultAPIKeyLabel) \(_apiKey)"
+        apiKeyLabel.text = "\(defaultAPIKeyLabel) \(_apiManager.getAPIKey())"
     }
 
     //MARK: Actions
     @IBAction func trackPageView(_ sender: UIButton) {
+        _apiManager.pageView(data: [:])
     }
 }
 
