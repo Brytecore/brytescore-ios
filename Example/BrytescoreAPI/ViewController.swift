e7c84a32d43493e96aeb1f2c56848de25cb61208
Emily Morehouse <emily@cuttlesoft.com>
2017-08-18 16:07:00 -0600

    [#150019614] Adding button to toggle dev mode in example app.

--- Example/BrytescoreAPI/ViewController.swift
+++ Example/BrytescoreAPI/ViewController.swift
@@ -12,10 +12,12 @@ import BrytescoreAPI
 class ViewController: UIViewController {
     // -------------------------- MARK: Properties -------------------------- //
     @IBOutlet weak var apiKeyLabel: UILabel!
+    @IBOutlet weak var toggleDevModeButton: UIButton!
 
     // --------------------------- MARK: Variables -------------------------- //
     // Initialize the API Manager with your API key.
     let _apiManager = BrytescoreAPIManager(apiKey: "107e322f-3410-4d5b-8f43-d2bad8141771")
+    var devMode = true
     let defaultAPIKeyLabel = "Your API Key:"
 
     // ---------------------------- MARK: Methods --------------------------- //
@@ -23,7 +25,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Enable debug mode
-        _apiManager.devMode(enabled: true)
+        _apiManager.devMode(enabled: devMode)
+        toggleDevModeButton.setTitle("Toggle Dev Mode: Turn \(devMode ? "off": "on")", for: .normal)
 
         // Update the API Key label to show our API key for debugging
         apiKeyLabel.text = "\(defaultAPIKeyLabel) \(_apiManager.getAPIKey())"
@@ -37,5 +40,12 @@ class ViewController: UIViewController {
     @IBAction func trackRegisteredAccount(_ sender: UIButton) {
         _apiManager.registeredAccount(data: ["isLead": false as AnyObject])
     }
+    
+    @IBAction func toggleDevMode(_ sender: UIButton) {
+        devMode = !devMode
+        _apiManager.devMode(enabled: devMode)
+        sender.setTitle("Toggle Dev Mode: Turn \(devMode ? "off": "on")", for: .normal)
+    }
+    
 
 }
