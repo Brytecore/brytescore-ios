45a4d56c308d1634bc3f30cbd14e2f00b527ef65
Emily Morehouse <emily@cuttlesoft.com>
2017-08-20 12:44:11 -0600

    [#150372214] Updating tests. Currently only tests initialization (API key).

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,27 +3,21 @@ import XCTest
 import BrytescoreAPI
 
 class Tests: XCTestCase {
-    
+
+    let _apiManager = BrytescoreAPIManager(apiKey: "fakefakefake")
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        _apiManager.devMode(enabled: true)
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testInitialization() {
+        XCTAssertNotNil(_apiManager, "Cannot find BrytescoreAPIManager instance");
+        XCTAssertEqual(_apiManager.getAPIKey(), "fakefakefake", "API key was not set correctly")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
